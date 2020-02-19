5cdb4e8e46406ce9f4a95e1308c611ef80902c53
Mannwel <52308885+Mannwel@users.noreply.github.com>
2020-02-20 00:51:25 +0900

    Update HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hellow World");
 		System.out.println("I'm adding code to my project.");
+		System.out.println("I'm adding code from GitHub.");
 	}
 }
